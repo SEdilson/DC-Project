eeb7e45ee152ce25fe1bfd5dd26ec6cb5b412fdb
SEdilson <edilson.silva00@hotmail.com>
2018-06-27 21:17:43 -0300

    Mudando para let a variável do bloco

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ var menu = document.querySelector('.fa-bars');
 
 menu.addEventListener("click", () => {
 
-    var navegacao = document.querySelector('.menu-nav');
+    let navegacao = document.querySelector('.menu-nav');
     if(navegacao.style.display === 'none')
         navegacao.style.display = 'block';
     else {
